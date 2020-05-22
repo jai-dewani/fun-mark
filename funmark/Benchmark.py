340afcf3acdf5741928ac7d943c6bc55d952298f
jai-dewani <jai.dewani.99@gmail.com>
2020-05-22 19:48:20 +0530

    Create different plotting functions for time and memory

--- funmark/Benchmark.py
+++ funmark/Benchmark.py
@@ -12,6 +12,11 @@ class Benchmark:
         self.functionName = None
 
     def run(self, func, *argv):
+        """
+        return:
+        runtime: int, the time it took to run (seconds)
+        memory: int, total memeory took by the function (KB)
+        """
         self.functionName = func.__name__
         tracemalloc.start()
         start = t.time()
@@ -21,17 +26,18 @@ class Benchmark:
         __,peak = tracemalloc.get_traced_memory()
         tracemalloc.stop()
         runtime = end - start
-        print(f'The runtime for {func.__name__} took {runtime} seconds to complete')
-        return runtime, peak/10**6
+        print(f'The runtime for {func.__name__} took {runtime} seconds to complete with {peak/10**3}KB memory')
+        return runtime, peak/10**3
 
     def add(self, index, time=None, memory=None):
-        if time:
+        if time!=None:
             self.time.append([index, time])
-        if memory:
+        if memory!=None:
             self.memory.append([index, memory])
     
-    def plotGraph(self, xlabel="", ylabel="", title="", label="", show=True, plt=plt):
+    def plotTime(self, xlabel="", ylabel="", title="", label="", show=True, plt=plt):
         time = np.array(self.time)
+        print('time',time)
         plt.plot(time[:,0],time[:,1], label=label)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
@@ -40,11 +46,28 @@ class Benchmark:
             plt.show()
         return plt
 
-    def compare(self, xlabel="", ylabel="", title="", *argv):
-        temp = self.plotGraph(xlabel=xlabel, ylabel=ylabel, title=title, label=self.functionName, show=False, plt=plt)
+    def plotMemory(self, xlabel="", ylabel="", title="", label="", show=True, plt=plt):
+        memory = np.array(self.memory)
+        print('memory',memory)
+        plt.plot(memory[:,0],memory[:,1], label=label)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        if show:
+            plt.show()
+        return plt
+
+    def compareTime(self, xlabel="", ylabel="", title="", *argv):
+        time_plt = self.plotTime(xlabel=xlabel, ylabel=ylabel, title=title, label=self.functionName, show=False, plt=plt)
         for other in argv:
-            # time2 = np.array(other.time, plt=temp)
-            # plt.plot(time2[:,0],time2[:,1])
-            other.plotGraph(xlabel=xlabel, ylabel=ylabel, title=title, label=other.functionName, show=False, plt=temp)
-        temp.legend(loc="upper right")
-        temp.show()
+            other.plotTime(xlabel=xlabel, ylabel=ylabel, title=title, label=other.functionName, show=False, plt=time_plt)
+        time_plt.legend(loc="upper left")
+        time_plt.show()
+            
+    
+    def compareMemory(self, xlabel="", ylabel="", title="", *argv):
+        memory_plt = self.plotMemory(xlabel=xlabel, ylabel=ylabel, title=title, label=self.functionName, show=False, plt=plt)
+        for other in argv:
+            other.plotMemory(xlabel=xlabel, ylabel=ylabel, title=title, label=other.functionName, show=False, plt=memory_plt)
+        memory_plt.legend(loc="upper left")
+        memory_plt.show()
